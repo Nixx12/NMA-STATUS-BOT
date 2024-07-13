5a2903ab8bea1ef8ad7bd558603372c3bd563ce0
Taylor Nixon <taylornixon03hotmail.co.uk@gmail.com>
2024-07-14 00:27:11 +0100

    QOL changes and extra packages for future development

--- NMA Status Bot/index.js
+++ NMA Status Bot/index.js
@@ -6,23 +6,26 @@ dotenv.config();
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-var date = new Date().toLocaleString();
 
 let updateStatusTimer;
+let currentStatusMessage = '';
 
 function setBotStatus(playerCount, serverCapacity, mapName, gamemode) {
-    const statusMessage = `${playerCount}/${serverCapacity} | ${mapName} | ${gamemode}\nDeveloped By Nixx`;
-    
+    const statusMessage = `${playerCount}/${serverCapacity} | ${mapName} | ${gamemode} - Developed By Nixx`
+    currentStatusMessage = statusMessage;
+
+    var date = new Date().toLocaleString();
     try {
         client.user.setActivity(`${statusMessage}`);
         console.log(`Bot status set to: ${statusMessage}`)
-        console.log(date)
+        console.log(`Current Time: ${date}`)
     } catch(error) {
-        console.log(`Error settings bot status, ${error}`)
+        console.log(`Error setting bot status: ${error}`)
         setTimeout(() => {
             startStatusUpdateTimer();
         }, 5000);
     }
+    return statusMessage;
 }
 
 function startStatusUpdateTimer() {
@@ -36,9 +39,9 @@ function startStatusUpdateTimer() {
         });
     }, 30000);
 }
+
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-
     startStatusUpdateTimer();
 });
 
